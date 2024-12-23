a458bafb9a9bda743d7628f6fe306a193d732b79
NickUsov1998 <nik_usov.1998@mail.ru>
2024-12-23 12:00:31 +0500

    added tests for sort functions tests moved from main.py to Sorts/tests

--- main.py
+++ main.py
@@ -24,22 +24,7 @@ def main() -> int:
         return 0
 
 if __name__ == '__main__':
-        list_1 = [5, 0, -2, 7, 3]
-        print(list_1)
-        bubble_sort(list_1)
-        print(list_1)
-        list_1 = [5, 0, -2, 7, 3]
-        select_sort(list_1)
-        print(list_1)
-        list_1 = [5, 0, -2, 7, 3]
-        gnome_sort(list_1)
-        print(list_1)
-        list_1 = [5, 0, -2, 7, 3]
-        merge_sort(list_1)
-        print(list_1)
-        tournament_list = [11,14,5,10,13,15,12,7,4,3,6,1,8,9,2]
-        tournament_sort(tournament_list)
-        print(tournament_list)
+        main()
 
 
 
